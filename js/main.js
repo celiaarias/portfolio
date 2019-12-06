a5b4ce10d32e1b56729cf181c8390e2254bfd090
celiaarias <58591681+celiaarias@users.noreply.github.com>
2019-12-06 11:27:25 +0100

    Crear repositorio

---
+++ js/main.js
@@ -0,0 +1,189 @@
+function myFunction() {
+    let graphicButton = document.getElementById("graphicButton");
+    let allButton = document.getElementById("allButton");
+    let webButton = document.getElementById("webButton");
+    let packagingButton = document.getElementById("packagingButton");
+    let productButton = document.getElementById("productButton");
+    let web = document.getElementById("web");
+    let graphic = document.getElementById("graphic");
+    let packaging = document.getElementById("packaging");
+    let all = document.getElementById("all");
+
+    allButton.style.color = 'black';
+    webButton.style.color = '#777777';
+    graphicButton.style.color = '#777777';
+    packagingButton.style.color = '#777777';
+    productButton.style.tcolor = '#777777';
+
+    web.style.display = "none";
+    graphic.style.display = 'none';
+    packaging.style.display = 'none';
+    product.style.display = 'none';
+    all.style.display = 'block';
+    AOS.refresh();
+  }
+  
+  function myFunction2() {
+    let graphicButton = document.getElementById("graphicButton");
+    let allButton = document.getElementById("allButton");
+    let webButton = document.getElementById("webButton");
+    let packagingButton = document.getElementById("packagingButton");
+    let productButton = document.getElementById("productButton");
+    let web = document.getElementById("web");
+    let graphic = document.getElementById("graphic");
+    let packaging = document.getElementById("packaging");
+    let all = document.getElementById("all");
+
+    allButton.style.color = '#777777';
+    webButton.style.color = '#777777';
+    graphicButton.style.color = 'black';
+    packagingButton.style.color = '#777777';
+    productButton.style.color = '#777777';
+    
+    web.style.display = 'none';
+    graphic.style.display = 'block';
+    packaging.style.display = 'none';
+    product.style.display = 'none';
+    all.style.display = 'none';
+    AOS.refresh();
+  }
+
+  function myFunction3() {
+    let graphicButton = document.getElementById("graphicButton");
+    let allButton = document.getElementById("allButton");
+    let webButton = document.getElementById("webButton");
+    let packagingButton = document.getElementById("packagingButton");
+    let productButton = document.getElementById("productButton");
+    let web = document.getElementById("web");
+    let graphic = document.getElementById("graphic");
+    let packaging = document.getElementById("packaging");
+    let all = document.getElementById("all");
+
+    allButton.style.color = '#777777';
+    webButton.style.color = 'black';
+    graphicButton.style.color = '#777777';
+    packagingButton.style.color = '#777777';
+    productButton.style.color = '#777777';
+    
+    web.style.display = "block";
+    graphic.style.display = 'none';
+    packaging.style.display = 'none';
+    product.style.display = 'none';
+    all.style.display = 'none';
+    AOS.refresh();
+  }
+  function myFunction4() {
+    let graphicButton = document.getElementById("graphicButton");
+    let allButton = document.getElementById("allButton");
+    let webButton = document.getElementById("webButton");
+    let packagingButton = document.getElementById("packagingButton");
+    let productButton = document.getElementById("productButton");
+    let web = document.getElementById("web");
+    let graphic = document.getElementById("graphic");
+    let packaging = document.getElementById("packaging");
+    let all = document.getElementById("all");
+
+    allButton.style.color = '#777777';
+    webButton.style.color = '#777777';
+    graphicButton.style.color = '#777777';
+    packagingButton.style.color = 'black';
+    productButton.style.color = '#777777';
+    
+    web.style.display = "none";
+    graphic.style.display = 'none';
+    packaging.style.display = 'block';
+    product.style.display = 'none';
+    all.style.display = 'none';
+    AOS.refresh();
+  }
+
+  function myFunction5() {
+    let graphicButton = document.getElementById("graphicButton");
+    let allButton = document.getElementById("allButton");
+    let webButton = document.getElementById("webButton");
+    let packagingButton = document.getElementById("packagingButton");
+    let productButton = document.getElementById("productButton");
+    let web = document.getElementById("web");
+    let graphic = document.getElementById("graphic");
+    let packaging = document.getElementById("packaging");
+    let all = document.getElementById("all");
+
+    allButton.style.color = '#777777';
+    webButton.style.color = '#777777';
+    graphicButton.style.color = '#777777';
+    packagingButton.style.color = '#777777';
+    productButton.style.color = 'black';
+    
+    web.style.display = "none";
+    graphic.style.display = 'none';
+    packaging.style.display = 'none';
+    product.style.display = 'block';
+    all.style.display = 'none';
+    AOS.refresh();
+  }
+//   function myFunction2() {
+//     let uxUi = document.getElementById("ux-ui");
+//     let graphicButton = document.getElementById("graphicButton");
+//     let allButton = document.getElementById("allButton");
+//     let webButton = document.getElementById("uxUiButton");
+//     let illustrationButton = document.getElementById("illustrationButton");
+//     let graphic = document.getElementById("graphic-design");
+//     let illustration = document.getElementById("illustration");
+//     uxUi.style.display = "flex";
+//     graphic.style.display = 'none';
+//     illustration.style.display = 'none';
+//     uxUiButton.style.textDecoration = 'underline wavy #D4273E';
+//     allButton.style.textDecoration = 'none';
+//     graphicButton.style.textDecoration = 'none';
+//     illustrationButton.style.textDecoration = 'none';
+//     AOS.refresh();
+//   }
+//   function myFunction3() {
+//     let graphic = document.getElementById("graphic-design");
+//     let graphicButton = document.getElementById("graphicButton");
+//     let allButton = document.getElementById("allButton");
+//     let uxUiButton = document.getElementById("uxUiButton");
+//     let illustrationButton = document.getElementById("illustrationButton");
+//     let uxUi = document.getElementById("ux-ui");
+//     let illustration = document.getElementById("illustration");
+//     graphic.style.display = 'flex';
+//     graphicButton.style.textDecoration = 'underline wavy #D4273E';
+//     illustrationButton.style.textDecoration = 'none';
+//     uxUiButton.style.textDecoration = 'none';
+//     allButton.style.textDecoration = 'none';
+//     uxUi.style.display = 'none';
+//     illustration.style.display = 'none';
+//     AOS.refresh();
+//   }
+//   function myFunction4() {
+//     let illustration = document.getElementById("illustration");
+//     let graphicButton = document.getElementById("graphicButton");
+//     let allButton = document.getElementById("allButton");
+//     let uxUiButton = document.getElementById("uxUiButton");
+//     let illustrationButton = document.getElementById("illustrationButton");
+//     let uxUi = document.getElementById("ux-ui");
+//     let graphic = document.getElementById("graphic-design");
+//     illustration.style.display = 'flex';
+//     illustrationButton.style.textDecoration = 'underline wavy #D4273E';
+//     allButton.style.textDecoration = 'none';
+//     uxUiButton.style.textDecoration = 'none';
+//     graphicButton.style.textDecoration = 'none';
+//     uxUi.style.display = 'none';
+//     graphic.style.display = 'none';
+//     AOS.refresh();
+//     console.log(AOS)
+//   }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
